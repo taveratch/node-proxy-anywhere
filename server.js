11202273fa3607bf954eaa6f6896d25877ccd176
taweesoft <taweesoft@gmail.com>
2017-09-21 22:15:58 +0700

    add proxy-server

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import proxy from 'http-proxy-middleware';
+import cors from 'cors';
+import url from 'url';
+import config from './config.json';
+
+let app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(cors());
+
+const getQuery = originalUrl => {
+    let index = originalUrl.indexOf('q=');
+    return originalUrl.substring(index+2);
+};
+
+let options = {
+    target: config.host,
+    router: (req) => {
+        let q = getQuery(req.originalUrl);
+        let parsedUrl = url.parse(q);
+        let target = `${parsedUrl.protocol}//${parsedUrl.host}`;
+        console.log('Target: ' + target);
+        return target;
+    },
+    pathRewrite: (path, req) => {
+        let q = getQuery(req.originalUrl);
+        let parsedUrl = url.parse(q);
+        let urlPath = parsedUrl.path;
+        console.log('Path: ' + urlPath);
+        return urlPath;
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        let q = getQuery(req.originalUrl);
+        console.log('Proxying to ' + q);
+        console.log('------');
+    },
+    changeOrigin: true
+};
+
+app.use('/api', proxy(options));
+app.listen(PORT, () => {
+    console.log(`Proxy server is running on port ${PORT}`);
+});
